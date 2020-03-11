0d0fb95709ac3beec90820f60e094ebcff54af1e
Benjamin Ko <benjamintko@gmail.com>
2020-03-11 23:05:47 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -18,7 +18,7 @@ def turn(board)
   puts "Please enter 1-9:"
   input = gets.strip
   index = input_to_index(input)
-  while valid_move?(board, index) == false
+  while valid_move?(board, index) == true
   puts "Please enter 1-9:"
 end
     move(board, index)
