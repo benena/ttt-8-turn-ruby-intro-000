c2488cd0c225731685184d258a97c95df8e6cb98
Benjamin Ko <benjamintko@gmail.com>
2020-03-11 22:52:59 +0000

    Automatically backed up by Learn

---
+++ lib/turn.rb
@@ -0,0 +1,38 @@
+def display_board(board)
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
+
+def input_to_index(input)
+  input = input.to_i - 1
+end
+
+def move(array, index, me = "X")
+  array[index] = me
+end
+
+def turn(board)
+  puts "Please enter 1-9:"
+  input = gets.strip
+  if input_to_index(input) == true
+  else while valid_move?(board, index) == false
+    puts "Please enter 1-9:"
+  end
+end
+
+def valid_move?(board, index)
+  if position_taken?(board, index) == false && index.between?(0,8) == true
+    true
+end
+end
+
+def position_taken?(board, index)
+  if board[index] == "" || board[index] == " " || board[index] == nil
+    false
+  else board[index] == "X" || board[index] == "O"
+    true
+  end
+end
